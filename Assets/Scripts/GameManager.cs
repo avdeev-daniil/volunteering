1390cefad30b68b72b6ddf9dc44f9bd216abebf1
avdeev-daniil <2027avdeev.dv@student.letovo.ru>
2026-06-02 21:45:38 +0300

    Version Three: First with graphic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,50 +7,54 @@ public class GameManager : MonoBehaviour
 {
     public GameObject buttonPrefab;
     public Transform buttonParent;
-
+    public List<GameObject> backgrounds = new List<GameObject>();
     private int currentLevel = 0;
+    private int firstClick = 0;
+    private List<GameObject> svetoforishe = new List<GameObject>();
+    private float timer_1 = 0;
 
     private List<GameObject> spawnedButtons =
         new List<GameObject>();
 
     public List<LevelsScript> levels = new List<LevelsScript>();
 
+    public GameObject traficLight;
     public TMP_Text score;
     public int points = 0;
 
     public TMP_Text time;
-    public float counter = 15f;
+    public int counter = 15;
 
     private Vector2[][] levelPositions =
     {
         new Vector2[]
         {
-            new Vector2(-200, 100),
-            new Vector2(200, 100)
+            new Vector2(-311, 76),
+            new Vector2(314, 76)
         },
 
         new Vector2[]
         {
-            new Vector2(-200, 100),
+            new Vector2(357, 106),
+            new Vector2(-345, 106),
+            new Vector2(0, 172)
+        },
+
+        new Vector2[]
+        {
+            new Vector2(222, 167),
+            new Vector2(-214, 167),
+            new Vector2(-241, -112),
+            new Vector2(250, -112)
+        },
+
+        new Vector2[]
+        {
+            new Vector2(-270, -117),
+            new Vector2(273, -117),
+            new Vector2(311, 115),
             new Vector2(0, 200),
-            new Vector2(200, 100)
-        },
-
-        new Vector2[]
-        {
-            new Vector2(-300, 100),
-            new Vector2(100, 200),
-            new Vector2(300, 100),
-            new Vector2(-100, 200)
-        },
-
-        new Vector2[]
-        {
-            new Vector2(300, 100),
-            new Vector2(0, 200),
-            new Vector2(-300, 100),
-            new Vector2(200, 200),
-            new Vector2(-200, 200)
+            new Vector2(-303, 126)
         }
     };
 
@@ -59,14 +63,24 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         ShuffleLevels(levels);
+        CreateTraficLight();
         LoadLevel();
     }
 
     void Update()
     {
-        counter = counter - Time.deltaTime;
-        time.text = $"Время: {counter}";
-        if (counter <= 0){
+        timer_1 += Time.deltaTime;
+        if (timer_1 > 1)
+        {
+            counter = counter - 1;
+            timer_1 = 0f;
+        }
+        //time.text = $"Время: {counter}";
+        if (counter % 3 == 0 && counter != 15){
+            svetoforishe[counter / 3].SetActive(false);
+        }
+        if (counter == 0)
+        {
             UnityEditor.EditorApplication.isPlaying = false;
         }
     }
@@ -76,6 +90,7 @@ public class GameManager : MonoBehaviour
         ClearButtons();
 
         List<string> numbers = levels[currentLevel].answers;
+
 
         Vector2[] positions =
             levelPositions[numbers.Count - 2];
@@ -87,6 +102,16 @@ public class GameManager : MonoBehaviour
             new List<string>(numbers);
 
         Shuffle(shuffledNumbers);
+        Vector3 newpos = new Vector3(0, 0, 0);
+        GameObject currentBH = GameObject.FindWithTag("Background");
+        if (firstClick != 0)
+        {
+            Destroy(currentBH);
+        }
+        else{
+            firstClick = 1;
+        }
+        Instantiate(backgrounds[numbers.Count - 2], newpos, Quaternion.identity);
 
         for (int i = 0; i < shuffledNumbers.Count; i++)
         {
@@ -117,13 +142,14 @@ public class GameManager : MonoBehaviour
 
             points = points + 1;
 
-            score.text = $"Счёт: {points}";
+            score.text = $"{points}";
 
             currentLevel++;
 
             if (currentLevel >= levels.Count)
             {
                 Debug.Log("Игра пройдена!");
+                UnityEditor.EditorApplication.isPlaying = false;
                 return;
             }
 
@@ -136,7 +162,7 @@ public class GameManager : MonoBehaviour
                 points = points - 1;
             }
 
-            score.text = $"Счёт: {points}";
+            score.text = $"{points}";
             Debug.Log("Неправильно!");
         }
     }
@@ -174,6 +200,15 @@ public class GameManager : MonoBehaviour
             LevelsScript temp = list[i];
             list[i] = list[randomIndex];
             list[randomIndex] = temp;
+        }
+    }
+
+    void CreateTraficLight()
+    {
+        for (int i = 0; i < 5; i++){
+            Vector3 newpos = new Vector3(5.6f + i * 0.9f , -4.5f, 0);
+            GameObject jeden = Instantiate(traficLight, newpos, Quaternion.identity);
+            svetoforishe.Add(jeden);
         }
     }
 }
